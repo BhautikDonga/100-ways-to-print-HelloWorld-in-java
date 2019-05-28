fc1a62c2cbf233ae85e2337f4b27f81e075ef114
Bhautik Donga <34915439+BhautikDonga@users.noreply.github.com>
2019-05-28 19:14:44 +0530

    HelloWorld using InstanceBlock

---
+++ HelloWorld_InstanceBlock.java
@@ -0,0 +1,9 @@
+class HelloWorld_InstanceBlock{
+    //instance block
+    {
+        System.out.println("Hello, World!");
+    }
+    public static void main(String[] args){
+        HelloWorld_InstanceBlock obj = new HelloWorld_InstanceBlock();
+    }
+}
